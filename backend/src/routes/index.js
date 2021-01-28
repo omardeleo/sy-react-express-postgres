64c0e875c8db50d2dde21bb168c8fb7a7325a28a
Omar De Leo <omar.deleo@gmail.com>
2021-01-28 12:01:14 -0500

    Add routes to list Localstack s3 bucket contents and upload files

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const aws = require('aws-sdk');
+const { promisify } = require('util');
+const fs = require('fs');
+const path = require('path');
+
 const db = require("../db/models");
 
 const router = express.Router();
@@ -19,6 +24,50 @@ const createCounter = async () => {
   });
   return counter.count;
 }
+
+const config = {
+  accessKeyId: 'abc',
+  secretAccessKey: '123',
+  endpoint: 'http://localstack:4566',
+  s3ForcePathStyle: true,
+  ACL: 'public-read',
+}
+
+router.get('/api/v1/files', async function(req, res, next) {
+  const s3 = new aws.S3(config);
+  s3.createBucket = promisify(s3.createBucket);
+  s3.listObjects = promisify(s3.listObjects);
+  await s3.createBucket({Bucket: 'test-bucket'});
+  const data = await s3.listObjects({Bucket: 'test-bucket'});
+
+  res.json(data);
+});
+
+router.get('/api/v1/files/upload/:file', async function(req, res, next) {
+  const s3 = new aws.S3(config);
+  const uploadFile = async (fileName) => {
+    const fileContent = fs.readFileSync(fileName);
+
+    // Setting up S3 upload parameters
+    const params = {
+        Bucket: 'test-bucket',
+        Key: `upload-${Date.now()}.jpg`, // File name you want to save as in S3
+        Body: fileContent
+    };
+
+    // Uploading files to the bucket
+    s3.upload(params, function(err, data) {
+        if (err) {
+            throw err;
+        }
+        console.log(`File uploaded successfully. ${data.Location}`);
+    });
+  };
+  const dirPath = path.join(__dirname, req.params.file);
+  await uploadFile(dirPath);
+
+  res.redirect('/api/v1/files/')
+});
 
 /* GET home page. */
 router.get('/api/v1/', async function(req, res, next) {
