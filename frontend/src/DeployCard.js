116f48ccf870a4747f04e3653e1b95768753921c
Omar De Leo <omar.deleo@gmail.com>
2021-02-05 15:20:36 -0500

    Update title

--- frontend/src/DeployCard.js
+++ frontend/src/DeployCard.js
@@ -4,7 +4,7 @@ import logo from './logo.png';
 function DeployCard(props) {
   return (
     <Card className={props.classes.card}>
-      <h2 className={props.classes.cardHeader}>Deploy your app</h2>
+      <h2 className={props.classes.cardHeader}>Deploy your app to the cloud</h2>
       <p>Easily deploy your app using Shipyard.</p>
       <Box display="flex" justifyContent="center" pt="20px" mb="40px">
         <Link href="https://shipyard.build" target="_blank" rel="noopener">
