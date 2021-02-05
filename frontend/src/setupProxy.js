de301481585704ff69fedb37611697e66a0248fc
Omar De Leo <omar.deleo@gmail.com>
2021-02-05 15:19:31 -0500

    Split proxies to address ECONNRESET error on fetch

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -2,17 +2,6 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    '/api/*',
-    createProxyMiddleware({
-      target: 'http://backend:8080',
-      headers: {
-        "Connection": "keep-alive"
-      },
-      changeOrigin: true,
-      secure: false
-    })
-  );
-  app.use(
     '/test-bucket',
     createProxyMiddleware({
       'target': 'http://localstack:4566',
