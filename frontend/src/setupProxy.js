011b24ef4a1c29a0a0a403b12c043ab9c7557b68
Omar De Leo <omar.deleo@gmail.com>
2021-02-08 10:48:18 -0500

    Consolidate all proxy logic into setupProxy.js

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -2,14 +2,15 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://backend:8080',
+    })
+  );
+  app.use(
     '/test-bucket',
     createProxyMiddleware({
-      'target': 'http://localstack:4566',
-      headers: {
-        "Connection": "keep-alive"
-      },
-      changeOrigin: true,
-      secure: false
+      target: 'http://localstack:4566',
     })
   );
 };
